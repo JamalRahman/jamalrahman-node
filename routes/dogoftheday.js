b0d8d5c718fb9736dbb7ec1e88bddc1e555346ab
jamalrahman <jamalrahman95@gmail.com>
2018-01-18 02:42:53 +0000

    dotd

--- routes/dogoftheday.js
+++ routes/dogoftheday.js
@@ -1,13 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+
 var date = new Date();
-/* GET users listing. */
+var fs = require('fs');
+var commentUrl;
+
+fs.readFile('../dotd.txt', 'utf8', function(err, data) {
+    if (err) throw err;
+    commentUrl = data;
+});
+
 router.get('/', function(req, res, next) {
   res.render('dogoftheday', {
      title:"Dog of the Day",
      date:date.getTime(),
-     commentUrl:"loltest"
+     commentUrl:"https://reddit.com/"
   })
 });
 
