c79bf2c1d47ee6cd8e50356f8be21fedf7487175
jamalrahman <jamalrahman95@gmail.com>
2018-01-18 03:00:13 +0000

    Added correct link to Dog of the Day

--- routes/dogoftheday.js
+++ routes/dogoftheday.js
@@ -5,18 +5,18 @@ var router = express.Router();
 
 var date = new Date();
 var fs = require('fs');
-var commentUrl;
+var url;
 
 fs.readFile('dotd.txt', 'utf8', function(err, data) {
     if (err) throw err;
-    commentUrl = data;
+    url = data;
 });
 
 router.get('/', function(req, res, next) {
   res.render('dogoftheday', {
      title:"Dog of the Day",
      date:date.getTime(),
-     commentUrl:"https://reddit.com/"
+     commentUrl: commentUrl
   })
 });
 
