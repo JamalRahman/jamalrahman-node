059765f9352e76f94d7041fd859849ee2d1fba0c
jamalrahman <jamalrahman95@gmail.com>
2020-10-26 00:25:40 +0000

    Updated splash screen

--- routes/index.js
+++ routes/index.js
@@ -9,12 +9,6 @@ var projects = [
       description: "The dissertation for my Masters degree. This project develops a robust multi-agent optimisation system in Java capable of simulating traffic evacuations on arbitrary graphed road networks, and evaluating evacuation performance over configurable scenarios."
    },
    {
-      name: "Group Chat Simulator",
-      link: "https://github.com/JamalRahman/fb_messenger_sim",
-      image: '../images/splash-swarm.png',
-      description: "This NLP multi-agent project simulates a large messenger group chat containing over 100 agents, whose conversational models have been trained using Hidden Markov Models. Agents mimic their real-world counterparts, making conversation based on learned behaviours from 100,000+ training data points."
-   },
-   {
       name: "C++ Rasterizer",
       link: "https://github.com/jamalrahman/closedgl",
       image: '../images/splash-swarm.png',
@@ -22,16 +16,23 @@ var projects = [
 
    },
    {
-      name: "HTTP Web Server",
-      link: "https://github.com/jamalrahman/httpServer",
+      name: "Hybrid TFIDF",
+      link: "https://github.com/jamalrahman/hybridtfidf",
       image: '../images/splash-swarm.png',
-      description: "A fully functional, multithreaded, HTTP web server. Written in pure C as an exercise in understanding networking architectures from base-principles, praised for its efficiency & cleanliness."
+      description: "A Python implementation of the Hybrid-TFIDF algorithm This is an implementation of the Hybrid TF-IDF algorithm as proposed by David Ionuye and Jugal K. Kalita (2011)."
    },
    {
       name: "Memescraper",
       link: "https://github.com/jamalrahman/memescraper",
       image: "../images/splash-memescraper.png",
       description:"Memescraper is the desktop web-scraping application that mass-downloads reddit content.<br/>Choose your subreddit, choose how many images you want to download and go!"
+
+   },
+   {
+      name: "Find A Game - Recommendation",
+      link: "https://github.com/JamalRahman/recommender",
+      image: "../images/splash-memescraper.png",
+      description:"Video Game recommendation system & genre-exploration using vector embeddings"
 
    }
 ];
